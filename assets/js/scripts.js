a423252766021889cc34fe41dbe1ca22d6f4bcdf
1TechGen <bobmanjesh@gmail.com>
2025-05-09 12:07:21 +0530

    Create scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+// Common JavaScript functions can go here
+document.addEventListener('DOMContentLoaded', function() {
+    // Tooltip initialization
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+    
+    // Any other common JS functionality
+});
