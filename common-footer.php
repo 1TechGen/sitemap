fa00013f4a8cba85905f0e9c6b9af92713dc1620
1TechGen <bobmanjesh@gmail.com>
2025-05-09 12:04:39 +0530

    Create common-footer.php

---
+++ common-footer.php
@@ -0,0 +1,32 @@
+    </div> <!-- Close container from header -->
+
+    <footer class="bg-dark text-white mt-5 py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h5>About Sitemap Tools</h5>
+                    <p>Free online tools for webmasters and SEO professionals.</p>
+                </div>
+                <div class="col-md-6 text-md-end">
+                    <h5>Quick Links</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="/" class="text-white">Home</a></li>
+                        <li><a href="/tools/sitemap-generator.php" class="text-white">Sitemap Generator</a></li>
+                        <li><a href="/tools/sitemap-validator.php" class="text-white">Sitemap Validator</a></li>
+                        <li><a href="/tools/seo-optimize.php" class="text-white">SEO Optimizer</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="text-center mt-3">
+                <p>&copy; <?php echo date('Y'); ?> Sitemap Tools. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <!-- Custom JS -->
+    <script src="/assets/js/scripts.js"></script>
+</body>
+</html>
